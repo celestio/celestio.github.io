9b44dff2ac5de34626d1623fbc6d3de461ad99be
celestio <jake.love.mail@gmail.com>
2015-09-24 09:06:30 +0100

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -1,11 +1,6 @@
-function draw() 
-{
-	var c = document.getElementById("SideBar");
-	var ctx = c.getContext("2d");
-	ctx.fillStyle = "#FF0000";
-	ctx.fillRect(0,0,150,75);
-	ctx.canvas.height = window.innerHeight;
-	ctx.canvas.width = window.innerWidth;
-}
-
-draw();
+var c = document.getElementById("SideBar");
+var ctx = c.getContext("2d");
+ctx.canvas.height = window.innerHeight;
+ctx.canvas.width = window.innerWidth;
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(0,0,150,75);
