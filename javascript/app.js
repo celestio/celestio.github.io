16ca117c9a01088bff49f707dd1475687bbb2928
AllYouNeedIsLove <jake.love.mail@gmail.com>
2015-11-03 19:17:26 +0000

    ADDDED PLANETS

--- javascript/app.js
+++ javascript/app.js
@@ -4,23 +4,36 @@ var canvas = document.getElementById("map");
 var xslider = document.getElementById("xSlider");
 var yslider = document.getElementById("ySlider");
 var zslider = document.getElementById("zSlider");
-var graph = document.getElementById("date");
+var graph = document.getElementById("scale");
 var ctx = canvas.getContext("2d");
 
 var pixl = ctx.createImageData(1,1);
 var d  = pixl.data;  
 var curve = []
 
-function curveGen(){
+var mercury = {meanDistance:0.38709893, eccentricity:0.20563069, inclination:7.00487, longitudeOfAscending:48.33167, longitudeOfPerigee:77.45645, meanLongitude:252.25084, meanLongitudeCoef:538101628.29}
+var venus = {meanDistance:0.72333199, eccentricity:0.00677323, inclination:3.39471, longitudeOfAscending:76.68069, longitudeOfPerigee:131.53298, meanLongitude:181.97973, meanLongitudeCoef:210664136.06}
+var earth = {meanDistance:1.00000011, eccentricity:0.01671022, inclination:0.00005, longitudeOfAscending:-11.26064, longitudeOfPerigee:102.94719, meanLongitude:100.46435, meanLongitudeCoef:129597740.63}
+var mars = {meanDistance:1.52366231, eccentricity:0.09341233, inclination:1.85061, longitudeOfAscending:-49.57854, longitudeOfPerigee:336.04084, meanLongitude:355.45332, meanLongitudeCoef:68905103.78}
+var jupiter = {meanDistance:5.20336301, eccentricity:0.04839266, inclination:1.30530, longitudeOfAscending:100.55615, longitudeOfPerigee:14.7538, meanLongitude:34.40438, meanLongitudeCoef:10925078.35}
+var saturn = {meanDistance:9.53707032, eccentricity:0.05415060, inclination:2.48446, longitudeOfAscending:113.71504, longitudeOfPerigee:92.43194, meanLongitude:49.94432, meanLongitudeCoef:4401052.95}
+var uranus = {meanDistance:19.19126393, eccentricity:0.04716771, inclination:0.76986, longitudeOfAscending:74.22988, longitudeOfPerigee:170.96424, meanLongitude:313.23218, meanLongitudeCoef:1542547.79}
+var neptune = {meanDistance:30.06896348, eccentricity:0.00858587 , inclination:1.76917, longitudeOfAscending:131.72169, longitudeOfPerigee:44.97135, meanLongitude:304.88003, meanLongitudeCoef:786449.21}
+var pluto = {meanDistance:39.48168677, eccentricity:0.24880766 , inclination:17.14175, longitudeOfAscending:110.30347, longitudeOfPerigee:224.06676, meanLongitude:238.92881, meanLongitudeCoef:522747.90}
+
+planets = [mercury, venus, earth, mars, jupiter, saturn, uranus, neptune, pluto]
+
+function refreshCurve(){
 	curve = []
-	for(x = -15; x < 15; x = x + 0.005){
-		curve.push([x*25,100*eval(graph.value),-10])
+	for(p = 0; p < planets.length; ++p){
+		for(x = 0; x < 365*earth.meanLongitudeCoef/planets[p].meanLongitudeCoef; x = x + 5 * planets[p].meanDistance^2){
+			curve.push(getPosition(planets[p], x))
+		}
 	}
-	for(x = -15; x < 15; x = x + 0.005){
-		curve.push([x*25,100*eval(graph.value),10])
-	}
+	rescaleCanvas()
 }
 
+refreshCurve()
 
 function rescaleCanvas(){
 	canvas.style.height = window.innerHeight;
@@ -84,56 +97,50 @@ function drawSeries(series, colour){
 	}
 }
 
-/*need a time => days function
 function getPosition(orbit, days){
-	double RADS = 0.017453292519943295;
-    double cy = days / 36525;
-    double meanDistance = entity.orbit.meanDistance a
-    double eccentricity = entity.orbit.eccentricity e
-    double inclination = entity.orbit.inclination* RADS; i
-    double longitudeOfAscending = entity.orbit.longitudeOfAscending * RADS; O
-    double longitudeOfPerigee = entity.orbit.longitudeOfPerigee * RADS; w
-    double meanLongitud = mod2pi(entity.orbit.meanLongitude) * RADS); L
+	var RADS = 0.017453292519943295;
+    var cy = days / 36525;
+    var meanDistance = orbit.meanDistance;
+    var eccentricity = orbit.eccentricity;
+    var inclination = orbit.inclination* RADS; 
+    var longitudeOfAscending = orbit.longitudeOfAscending * RADS; 
+    var longitudeOfPerigee = orbit.longitudeOfPerigee * RADS; 
+    var meanLongitude = mod2pi((orbit.meanLongitude + orbit.meanLongitudeCoef*cy/3600) * RADS); 
 
-    double M = mod2pi(L - w);
-    double E = M + ((e * Math.Sin(M)) * (1.0 + (e * Math.Cos(M))));
-    double E1 = 0.0;
+    var meanAnomaly = mod2pi(meanLongitude - longitudeOfPerigee);
+    var eccentricAnomalyApprox = meanAnomaly + eccentricity * Math.sin(meanAnomaly) * (1.0 + eccentricity * Math.cos(meanAnomaly));
+    var eccentricAnomaly = 0;
 
     do
     {
-        E1 = E;
-        E = E1 - (E1 - e * Math.Sin(E1) - M) / (1 - e * Math.Cos(E1));
+        eccentricAnomaly = eccentricAnomalyApprox;
+        eccentricAnomalyApprox = eccentricAnomaly - (eccentricAnomaly - eccentricity * Math.sin(eccentricAnomaly) - meanAnomaly) / (1 - eccentricity * Math.cos(eccentricAnomaly));
     }
-    while (Math.Abs(E - E1) > 0.0000000000001);
+    while (Math.abs(eccentricAnomaly - eccentricAnomalyApprox) > 0.0000000001);
 
-    double V = 2 * Math.Atan(Math.Sqrt((1 + e) / (1 - e)) * Math.Tan(0.5 * E1));
-    double R = (a * (1 - (e * e))) / (1 + (e * (Math.Cos(V))));
+    var trueAnomaly = 2 * Math.atan(Math.sqrt((1 + eccentricity) / (1 - eccentricity)) * Math.tan(0.5 * eccentricAnomaly));
+    var helioCentricRadius = (meanDistance * (1 - (eccentricity * eccentricity))) / (1 + (eccentricity * (Math.cos(trueAnomaly))));
 
-    double xh = R * (Math.Cos(O) * Math.Cos(V + w - O) - Math.Sin(O) * Math.Sin(V + w - O) * Math.Cos(i));
-    double yh = R * (Math.Sin(O) * Math.Cos(V + w - O) + Math.Cos(O) * Math.Sin(V + w - O) * Math.Cos(i));
-    double zh = R * (Math.Sin(V + w - O) * Math.Sin(i));
+    var x = helioCentricRadius * (Math.cos(longitudeOfAscending) * Math.cos(trueAnomaly + longitudeOfPerigee - longitudeOfAscending) - Math.sin(longitudeOfAscending) * Math.sin(trueAnomaly + longitudeOfPerigee - longitudeOfAscending) * Math.cos(inclination));
+    var y = helioCentricRadius * (Math.sin(longitudeOfAscending) * Math.cos(trueAnomaly + longitudeOfPerigee - longitudeOfAscending) + Math.cos(longitudeOfAscending) * Math.sin(trueAnomaly + longitudeOfPerigee - longitudeOfAscending) * Math.cos(inclination));
+    var z = helioCentricRadius * (Math.sin(trueAnomaly + longitudeOfPerigee - longitudeOfAscending) * Math.sin(inclination));
 
-    return new double[] { xh, yh, zh };
+    return [x*graph.value, y*graph.value, z*graph.value];
 }
 
 function mod2pi(x)
 {
-    double b = x / (2 * Math.PI);
-    double a = (2 * Math.PI) * (b - abs_floor(b));
+    var b = x / (2 * Math.PI);
+    var a = (2 * Math.PI) * (b - abs_floor(b));
     if (a < 0) a = (2 * Math.PI) + a;
     return a;
 }
 
-function abs_floor( x)
+function abs_floor(x)
 {
-    double r;
-    if (x >= 0.0) r = Math.Floor(x);
-    else r = Math.Ceiling(x);
+    var r;
+    if (x >= 0.0) r = Math.floor(x);
+    else r = Math.ceil(x);
     return r;
-}*/
-
-
-
-
-
+}
 rescaleCanvas()
