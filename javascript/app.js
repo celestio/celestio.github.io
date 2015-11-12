42eac32ea278c0c55c0e366c4ce3691dc09bae09
AllYouNeedIsLove <jake.love.mail@gmail.com>
2015-11-12 16:30:18 +0000

    scrolll support

--- javascript/app.js
+++ javascript/app.js
@@ -2,42 +2,36 @@ var canvas = document.getElementById("map");
 var xslider = document.getElementById("xSlider");
 var yslider = document.getElementById("ySlider");
 var zslider = document.getElementById("zSlider");
-var graph = document.getElementById("scale");
+var scale = document.getElementById("scale");
 var date = document.getElementById("date");
-
 var fastback = document.getElementById("fastback")
 var slowback = document.getElementById("slowback")
 var pause = document.getElementById("pause")
 var slowforward = document.getElementById("slowforward")
 var fastforward = document.getElementById("fastforward")
+var ctx = canvas.getContext("2d");
+var daysPerSecond = 365.251;
 
 fastback.onclick = function(){
-	daysPerSecond = -2
+	daysPerSecond = -500;
 }
 
 slowback.onclick = function(){
-	daysPerSecond = -1
+	daysPerSecond = -100;
 }
 
 pause.onclick = function(){
-	daysPerSecond = 0
+	daysPerSecond = 0;
 }
 
 slowforward.onclick = function(){
-	daysPerSecond = 1
+	daysPerSecond = 100;
 }
 
 fastforward.onclick = function(){
-	daysPerSecond = 2
+	daysPerSecond = 500;
 }
 
-var ctx = canvas.getContext("2d");
-
-
-var daysPerSecond = 365.251;
-
-
-//REOREDER WHEN DOING SQL
 function Orbit(colour, tag, meanDistance, eccentricity, inclination, longitudeOfAscending, longitudeOfPerigee, meanLongitude, meanLongitudeCoef, origin) {
 	this.tag = tag;
 	this.origin = origin;
@@ -51,20 +45,18 @@ function Orbit(colour, tag, meanDistance, eccentricity, inclination, longitudeOf
 	this.meanLongitudeCoef = meanLongitudeCoef;
 	this.path = []
 }
-//please do database shit
-var mercury = new Orbit([153,9,34,255], "x", 0.38709893, 0.20563069, 7.00487, 48.33167, 77.45645, 252.25084, 538101628.29, 0);
-var venus = new Orbit([118,16,37,255], "x", 0.72333199, 0.00677323, 3.39471, 76.68069, 131.53298, 181.97973, 210664136.06, 0);
-var earth = new Orbit([97,78,98,255], "earth", 1.00000011, 0.01671022, 0.00005, -11.26064, 102.94719, 100.46435, 129597740.63, 0);
-var mars = new Orbit([35,89,99,255], "x", 1.52366231, 0.09341233, 1.85061, -49.57854, 336.04084, 355.45332, 68905103.78, 0);
-var jupiter = new Orbit([53,176,181,255], "x", 5.20336301, 0.04839266, 1.30530, 100.55615, 14.7538, 34.40438, 10925078.35, 0);
-var saturn = new Orbit([153,9,34,255], "x", 9.53707032, 0.05415060, 2.48446, 113.71504, 92.43194, 49.94432, 4401052.95, 0);
-var uranus = new Orbit([118,16,37,255], "x", 19.19126393, 0.04716771, 0.76986, 74.22988, 170.96424, 313.23218, 1542547.79, 0);
-var neptune = new Orbit([97,78,98,255], "x", 30.06896348, 0.00858587 , 1.76917, 131.72169, 44.97135, 304.88003, 786449.21, 0);
-var pluto = new Orbit([35,89,99,255], "x", 39.48168677, 0.24880766 , 17.14175, 110.30347, 224.06676, 238.92881, 522747.90, 0);
+
+var mercury = new Orbit([255,255,0,255], "x", 0.38709893, 0.20563069, 7.00487, 48.33167, 77.45645, 252.25084, 538101628.29, 0);
+var venus = new Orbit([255,255,0,255], "x", 0.72333199, 0.00677323, 3.39471, 76.68069, 131.53298, 181.97973, 210664136.06, 0);
+var earth = new Orbit([255,255,0,255], "earth", 1.00000011, 0.01671022, 0.00005, -11.26064, 102.94719, 100.46435, 129597740.63, 0);
+var mars = new Orbit([255,255,0,255], "x", 1.52366231, 0.09341233, 1.85061, -49.57854, 336.04084, 355.45332, 68905103.78, 0);
+var jupiter = new Orbit([255,255,0,255], "x", 5.20336301, 0.04839266, 1.30530, 100.55615, 14.7538, 34.40438, 10925078.35, 0);
+var saturn = new Orbit([255,255,0,255], "x", 9.53707032, 0.05415060, 2.48446, 113.71504, 92.43194, 49.94432, 4401052.95, 0);
+var uranus = new Orbit([255,255,0,255], "x", 19.19126393, 0.04716771, 0.76986, 74.22988, 170.96424, 313.23218, 1542547.79, 0);
+var neptune = new Orbit([255,255,0,255], "x", 30.06896348, 0.00858587 , 1.76917, 131.72169, 44.97135, 304.88003, 786449.21, 0);
+var pluto = new Orbit([255,255,0,255], "x", 39.48168677, 0.24880766 , 17.14175, 110.30347, 224.06676, 238.92881, 522747.90, 0);
 
 var selectedOrbits = [mercury, venus, earth, mars, jupiter, saturn, uranus, neptune, pluto];
-
-
 
 Orbit.prototype.createOrbit = function() { //maybe add precision arg?
 	this.path = [];
@@ -87,39 +79,26 @@ function removeOrbit(orbitTag){
 	}
 }
 
-function redrawCanvas(time){
-	canvas.setAttribute("width", window.innerWidth - 250); 
-	canvas.setAttribute("height", window.innerHeight);
-	
-	prevFrameTime = time - prevTime
-	
-	fps = Math.round(1000/(prevFrameTime));
-	
-	ctx.fillStyle = 'rgb(250,250,250)';
-        ctx.font = "10pt Courier";
-        ctx.fillText(fps+"fps", 10, 20);
-	
-	
+function doScroll(e){
+    e = window.event || e;
+    var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
 
-	var start = new Date().getTime(); 
+    scale.value = (parseFloat(scale.value) * (delta * 0.15 + 1)).toString()
+    e.preventDefault();
+};
 
-	for (i = 0; i < selectedOrbits.length; ++i) {
-		drawOrbit(selectedOrbits[i].path, selectedOrbits[i].colour);
-		drawObject(selectedOrbits[i])
-	};
-	date.value = (parseFloat(date.value) + prevFrameTime*daysPerSecond/1000).toString()
-	var end = new Date().getTime();
-<<<<<<< HEAD
-	console.log(end - start);
+function initialisePage(){
+	if (window.addEventListener) {
+   		window.addEventListener("mousewheel", doScroll, false);
+    	window.addEventListener("DOMMouseScroll", doScroll, false);
+	} 
 
-	ctx.fillStyle = "white"
-    ctx.fillText(end - start + "ms", 20, 20);
-=======
+	else {
+    	window.attachEvent("onmousewheel", doScroll);
+	}
 
-	prevTime = time;
-	
-	window.requestAnimationFrame(redrawCanvas);
->>>>>>> origin/master
+	date.value = "1";
+	daysPerSecond = 0;
 }
 
 prevTime = 0;
--- javascript/draw.js
+++ javascript/draw.js
@@ -16,11 +16,11 @@ function drawOrbit(orbitPath, colour){
 	ctx.moveTo(transformedPath[point][0] + midx, transformedPath[point][1] + midy);	
 	ctx.beginPath();
 
-	ctx.lineTo(transformedPath[transformedPath.length - 1][0]*graph.value + midx, transformedPath[transformedPath.length - 1][1]*graph.value + midy)	
+	ctx.lineTo(transformedPath[transformedPath.length - 1][0]*scale.value + midx, transformedPath[transformedPath.length - 1][1]*scale.value + midy)	
 	while(point--){
-		ctx.lineTo(transformedPath[point][0]*graph.value + midx, transformedPath[point][1]*graph.value + midy)
+		ctx.lineTo(transformedPath[point][0]*scale.value + midx, transformedPath[point][1]*scale.value + midy)
 	}
-	ctx.lineTo(transformedPath[transformedPath.length - 1][0]*graph.value + midx, transformedPath[transformedPath.length - 1][1]*graph.value + midy)
+	ctx.lineTo(transformedPath[transformedPath.length - 1][0]*scale.value + midx, transformedPath[transformedPath.length - 1][1]*scale.value + midy)
 	ctx.stroke();	
 }
 
@@ -36,10 +36,42 @@ function drawObject(orbit){
 	position = transform([getPosition(orbit, date.value)]);
 	
 	ctx.beginPath();
-	ctx.arc(position[0][0]*graph.value + midx, position[0][1]*graph.value + midy, 10, 0, 2*Math.PI);
+	ctx.arc(position[0][0]*scale.value + midx, position[0][1]*scale.value + midy, 10, 0, 2*Math.PI);
 	ctx.strokeStyle="black";
 	ctx.stroke();
 	
 	ctx.fillStyle="rgba("+r+","+g+","+b+","+a+")";
 	ctx.fill();
+
+	//please redo
+	ctx.beginPath();
+	ctx.arc(midx, midy, 10, 0, 2*Math.PI);
+	ctx.strokeStyle="black";
+	ctx.stroke();
+	
+	ctx.fillStyle="rgba("+r+","+g+","+b+","+a+")";
+	ctx.fill();
+}
+
+function redrawCanvas(time){
+	canvas.setAttribute("width", window.innerWidth - 250); 
+	canvas.setAttribute("height", window.innerHeight);
+	
+	prevFrameTime = time - prevTime
+	
+	fps = Math.round(1000/(prevFrameTime));
+	
+	ctx.fillStyle = 'rgb(250,250,250)';
+    ctx.font = "10pt Courier";
+    ctx.fillText(fps+"fps", 10, 20);	
+
+	for (i = 0; i < selectedOrbits.length; ++i) {
+		drawOrbit(selectedOrbits[i].path, selectedOrbits[i].colour);
+		drawObject(selectedOrbits[i])
+	};
+	date.value = (parseFloat(date.value) + prevFrameTime*daysPerSecond/1000).toString()
+
+	prevTime = time;
+	
+	window.requestAnimationFrame(redrawCanvas);
 }
