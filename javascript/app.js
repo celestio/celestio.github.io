8872b2bfba794d6a54ae4709484288bef10405fc
celestio <jakeloverocks@gmail.com>
2015-12-13 11:52:21 +0000

    Update app.js

--- javascript/app.js
+++ javascript/app.js
@@ -1,132 +1,155 @@
+//http://ssd.jpl.nasa.gov/?sat_elem
+
+var MSPERDAY = 840000
+var VIEWDISTANCE = 100
+
 var canvas = document.getElementById("map");
 var scaleInput = document.getElementById("scale");
 var dateInput = document.getElementById("date");
-var fastback = document.getElementById("fastback")
-var slowback = document.getElementById("slowback")
-var pause = document.getElementById("pause")
-var slowforward = document.getElementById("slowforward")
-var fastforward = document.getElementById("fastforward")
+var speedInput = document.getElementById("speed")
 var ctx = canvas.getContext("2d");
 var prevTime = 0;
 var xr = 0;
 var yr = 0;
 var zr = 0;
-var daysPerSecond = 0;
+var daysPerSecond = 100;
 var mousedown = false;
 var scale = 600;
-var date = 0;
-var viewDistance = 100
+var date = new Date();
 
 scaleInput.oninput = function() {
   scale = scaleInput.value;
-};
-
+}
 dateInput.oninput = function() {
-  date = dateInput.value;
-};
-
-fastback.onclick = function(){daysPerSecond = -500;}
-slowback.onclick = function(){daysPerSecond = -100;}
-pause.onclick = function(){daysPerSecond = 0;}
-slowforward.onclick = function(){daysPerSecond = 100;}
-fastforward.onclick = function(){daysPerSecond = 365.2*60;}
-
-function Orbit(colour, tag, meanDistance, eccentricity, inclination, longitudeOfAscending, longitudeOfPerigee, meanLongitude, meanLongitudeCoef, origin) {
-	this.tag = tag;
-	this.origin = origin;
-	this.colour = colour;
-	this.meanDistance = meanDistance;
-	this.eccentricity = eccentricity;
-	this.inclination = inclination;
-	this.longitudeOfAscending = longitudeOfAscending;
-	this.longitudeOfPerigee = longitudeOfPerigee;
-	this.meanLongitude = meanLongitude;
-	this.meanLongitudeCoef = meanLongitudeCoef;
-	this.path = []
-	this.distance = 0
+  date.value = dateInput.value;
 }
+speedInput.oninput = function() {  
+  if(this.value == 0){
+    daysPerSecond = 0
+  }
+  else if(this.value > 0){
+    daysPerSecond = Math.pow(Math.E, this.value)
+  }
+  else{
+    daysPerSecond = -Math.pow(Math.E, -this.value)
+  }
+}
+
+function Orbit(colour, tag, meanDistance, eccentricity, inclination, longitudeOfAscending, longitudeOfPerigee, meanLongitude, meanLongitudeCoef, children) {
+  this.tag = tag;
+  this.children = children;
+  this.colour = colour;
+  this.meanDistance = meanDistance;
+  this.eccentricity = eccentricity;
+  this.inclination = inclination;
+  this.longitudeOfAscending = longitudeOfAscending;
+  this.longitudeOfPerigee = longitudeOfPerigee;
+  this.meanLongitude = meanLongitude;
+  this.meanLongitudeCoef = meanLongitudeCoef;
+  this.path = [];
+  this.distance = 0;
+  this.radius = 0;
+  this.screenX = 0;
+  this.screenY = 0;
+  this.X = 0;
+  this.Y = 0;
+  this.Z = 0;
+}
+
 Orbit.prototype.createOrbit = function() { //maybe add precision arg?
-	this.path = [];
-	for(d = 0; d < 365*earth.meanLongitudeCoef/this.meanLongitudeCoef; d = d + (earth.meanLongitudeCoef)/(this.meanLongitudeCoef)){
-		this.path.push(getPosition(this, d));
-	}
+  this.path = [];
+  for (d = 0; d < 375 * planets.earth.meanLongitudeCoef / this.meanLongitudeCoef; d = d + (planets.earth.meanLongitudeCoef) / (2 * this.meanLongitudeCoef)) {
+    this.path.push(getPosition(this, d));
+  }
 }
 
-var mercury = new Orbit([206,116,126,255], "x", 0.38709893, 0.20563069, 7.00487, 48.33167, 77.45645, 252.25084, 538101628.29, 0);
-var venus = new Orbit([109,64,71,255], "x", 0.72333199, 0.00677323, 3.39471, 76.68069, 131.53298, 181.97973, 210664136.06, 0);
-var earth = new Orbit([102,112,113,255], "earth", 1.00000011, 0.01671022, 0.00005, -11.26064, 102.94719, 100.46435, 129597740.63, 0);
-var mars = new Orbit([97,108,76,255], "x", 1.52366231, 0.09341233, 1.85061, -49.57854, 336.04084, 355.45332, 68905103.78, 0);
-var jupiter = new Orbit([227,239,153,255], "x", 5.20336301, 0.04839266, 1.30530, 100.55615, 14.7538, 34.40438, 10925078.35, 0);
-var saturn = new Orbit([206,116,126,255], "x", 9.53707032, 0.05415060, 2.48446, 113.71504, 92.43194, 49.94432, 4401052.95, 0);
-var uranus = new Orbit([109,64,71,255], "x", 19.19126393, 0.04716771, 0.76986, 74.22988, 170.96424, 313.23218, 1542547.79, 0);
-var neptune = new Orbit([102,112,113,255], "x", 30.06896348, 0.00858587 , 1.76917, 131.72169, 44.97135, 304.88003, 786449.21, 0);
-var pluto = new Orbit([97,108,76,255], "x", 39.48168677, 0.24880766 , 17.14175, 110.30347, 224.06676, 238.92881, 522747.90, 0);
-var selectedOrbits = [mercury, venus, earth, mars, jupiter, saturn, uranus, neptune, pluto]
+var planets = {
+  moon: new Orbit([255, 255, 255, 255], "moon", 0.00257356604, 0.0549006, 5.14, 125.08, 318.15, 100.46435, 3.0, ["mooon"]),
+  mooon: new Orbit([255, 255, 0, 255], "mooon", 0.00257356604, 0.0549006, 5.14, 125.08, 318.15, 100.46435, 4.5, []),
+  mercury: new Orbit([255, 0, 255, 255], "mercury", 0.38709893, 0.20563069, 7.00487, 48.33167, 77.45645, 252.25084, 4.09233875876, []),
+  venus: new Orbit([255, 0, 0, 255], "venus", 0.72333199, 0.00677323, 3.39471, 76.68069, 131.53298, 181.97973, 1.60213048307, []),
+  earth: new Orbit([0, 255, 255, 255], "earth", 1.00000011, 0.01671022, 0.00005, -11.26064, 102.94719, 100.46435, 0.963, ["moon"]),
+  mars: new Orbit([0, 255, 0, 255], "mars", 1.52366231, 0.09341233, 1.85061, -49.57854, 336.04084, 355.45332, 0.52403272293, []),
+  jupiter: new Orbit([0, 0, 255, 255], "jupiter", 5.20336301, 0.04839266, 1.30530, 100.55615, 14.7538, 34.40438, 0.08308676566, []),
+  saturn: new Orbit([255, 255, 0, 255], "saturn", 9.53707032, 0.05415060, 2.48446, 113.71504, 92.43194, 49.94432, 0.0334706311, []),
+  uranus: new Orbit([255, 0, 255, 255], "uranus", 19.19126393, 0.04716771, 0.76986, 74.22988, 170.96424, 313.23218, 0.01173129354, []),
+  neptune: new Orbit([0, 255, 0, 255], "neptune", 30.06896348, 0.00858587, 1.76917, 131.72169, 44.97135, 304.88003, 0.00598105709, []),
+  pluto: new Orbit([0, 255, 255, 255], "pluto", 39.48168677, 0.24880766, 17.14175, 110.30347, 224.06676, 238.92881, 0.00395169701, []),
+  sun: new Orbit([255, 255, 0, 255], "sun", 0, 0, 0, 0, 0, 0, 0, ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune", "pluto"])
+}
+var selectedOrbits = [planets.sun];
+var focusPlanet = planets.sun;
 
-function initialiseOrbits(){
-	for(i = 0; i < selectedOrbits.length; ++i){
-		selectedOrbits[i].createOrbit();
-	}
-	redrawCanvas(0);
+function initialiseOrbits(parent) {
+  parent.createOrbit();
+  for (var i = 0; i < parent.children.length; ++i) {
+    initialiseOrbits(planets[parent.children[i]]);
+  }
 }
 
-function removeOrbit(orbitTag){
-	for(i = 0; i < selectedOrbits.length; ++i){
-		if(selectedOrbits[i].tag == orbitTag){
-			orbits.splice(orbit, 1)
-		}
-	}
-}
+function doScroll(e) { //maybe add sensitivity arg?
+  e = window.event || e;
+  var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
 
-function doScroll(e){ //maybe add sensitivity arg?
-    e = window.event || e;
-    var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+  if (delta == -1) {
+    scale = scale * 0.8;
+  } else {
+    scale = scale * 1.25;
+  }
 
-    if(delta == -1){
-    	scale = scale * 0.8
-    }
-
-    else{
-    	scale = scale * 1.25
-    }
-    
-    e.preventDefault();
+  e.preventDefault();
 };
 
-function initialisePage(){
-
-	if (window.addEventListener){
-   		window.addEventListener("mousewheel", doScroll, false);
-    	window.addEventListener("DOMMouseScroll", doScroll, false);
-	}
-
-	else{
-    	window.attachEvent("onmousewheel", doScroll);
-	}
-
-	canvas.onmousedown = function(e){mousedown = true; prevMouseX = e.clientX/100; prevMouseY = e.clientY/100;};
-	canvas.onmouseup = function(){mousedown = false;};
-
-	canvas.onmousemove = function(e){
-		mouseX = e.clientX/100;
-		mouseY = e.clientY/100;
-
-		if(mousedown){
-			deltax = mouseX - prevMouseX;
-			deltay = mouseY - prevMouseY;
-			zr += deltax;
-			xr += deltay
-		}		
-
-		prevMouseX = mouseX;
-		prevMouseY = mouseY;
-	}
+function checkForPlanet(parent, x, y) {
+  for (var i = 0; i < parent.children.length; ++i) {
+    checkForPlanet(planets[parent.children[i]], x, y);
+  }
+  if(Math.abs(x - midx - 250 - parent.screenX) < (parent.radius + 7) & Math.abs(y - midy - parent.screenY) < (parent.radius + 7)){
+    focusPlanet = planets[parent.tag];
+  }
 }
 
-function syncvariables(){
-	dateInput.value = date | 0;
-	scaleInput.value = scale | 0;
+function initialisePage() {
+
+  if (window.addEventListener) {
+    window.addEventListener("mousewheel", doScroll, false);
+    window.addEventListener("DOMMouseScroll", doScroll, false);
+  } else {
+    window.attachEvent("onmousewheel", doScroll);
+  }
+
+  canvas.onmousedown = function(e) {
+    mousedown = true;
+    prevMouseX = e.clientX / 100;
+    prevMouseY = e.clientY / 100;
+    checkForPlanet(planets.sun, e.clientX, e.clientY)
+  };
+  canvas.onmouseup = function() {
+    mousedown = false;
+  };
+
+  canvas.onmousemove = function(e) {
+    mouseX = e.clientX / 100;
+    mouseY = e.clientY / 100;
+
+    if (mousedown) {
+      deltax = mouseX - prevMouseX;
+      deltay = mouseY - prevMouseY;
+      zr -= deltax * 0.75;
+      xr += deltay * 0.75;
+    }
+
+    prevMouseX = mouseX;
+    prevMouseY = mouseY;
+  }
 }
 
+function syncvariables() {
+  dateInput.value = date.toISOString();
+  scaleInput.value = scale | 0;
+}
+
+initialiseOrbits(planets.sun);
 syncvariables();
-initialiseOrbits();
+
+redrawCanvas(0)
